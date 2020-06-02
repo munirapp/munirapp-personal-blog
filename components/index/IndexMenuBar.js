0dcb35998d2d5e1b6562b730095aab02d23c3235
munirapp <softwaremakassar@gmail.com>
2020-06-03 00:10:06 +0800

    Unfinish set smooth scroll

--- components/index/IndexMenuBar.js
+++ components/index/IndexMenuBar.js
@@ -1,24 +1,47 @@
-export default function IndexMenuBar() {
-  const list_menu = [
-    { icon: "/image/user.svg", link: "#home" },
-    { icon: "/image/dialogue.svg", link: "#blog" },
-    { icon: "/image/gallery.svg", link: "#portofolio" },
-    { icon: "/image/flasks.svg", link: "#labs" },
-    { icon: "/image/drink.svg", link: "#hiring" },
-  ];
-  return (
-    <div className="menubar lg:animate__animated lg:animate__menubar lg:animate__delay-2s">
-      <ul>
-        {list_menu.map((item) => {
-          return (
-            <li>
-              <a href={item.link}>
-                <img src={item.icon} alt={item.link} loading="lazy" />
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+import React, { Component, useState} from "react";
+
+class IndexMenuBar extends Component {
+  const [list_item, set_list_item]
+  handleClick = (e) => {};
+  render() {
+    console.log(this.props);
+    return <div>test</div>;
+    return (
+      <div className="menubar">
+        <ul>
+          {list_menu.map((item) => {
+            return (
+              <li key={item.link}>
+                <a href={item.link}>
+                  <img
+                    className="animate__animated animate__zoomIn"
+                    src={item.icon}
+                    alt={item.link}
+                    loading="lazy"
+                    onClick={this.handleClick}
+                  />
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
+
+// export const getStaticProps = () => {
+//   return {
+//     props: {
+//       list_menu: [
+//         { icon: "/image/user.svg", link: "#home" },
+//         { icon: "/image/dialogue.svg", link: "#blog" },
+//         { icon: "/image/gallery.svg", link: "#portofolio" },
+//         { icon: "/image/flasks.svg", link: "#labs" },
+//         { icon: "/image/drink.svg", link: "#hiring" },
+//       ],
+//     },
+//   };
+// };
+
+export default IndexMenuBar;
