d3da07e08a14a944e6b0128d08fce8d2910ecf50
munirapp <softwaremakassar@gmail.com>
2020-07-03 22:20:08 +0800

    setup black & white theme 2

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,4 +1,24 @@
 export default function Blog() {
+  const handleClick = () => {
+    const light_theme = { color: "white", textColor: "black" };
+    const dark_theme = { color: "black", textColor: "white" };
+
+    const element = document.documentElement;
+    const selected_theme = getComputedStyle(element).getPropertyValue(
+      "--theme-color"
+    );
+
+    if (!selected_theme || selected_theme == "white") {
+      element.style.setProperty("--theme-color", "black");
+      element.style.setProperty("--theme-color-text", "white");
+    } else {
+      element.style.setProperty("--theme-color", "white");
+      element.style.setProperty("--theme-color-text", "black");
+    }
+
+    console.log(selected_theme);
+  };
+
   return (
     <div>
       <div className="container">
@@ -6,7 +26,9 @@ export default function Blog() {
           <a href="/">
             <span>Back to Home</span>
           </a>
-          <button className="btn btn-model2 ml-auto">Go to Night Mode</button>
+          <button className="btn btn-model2 ml-auto" onClick={handleClick}>
+            Go to Night Mode
+          </button>
         </div>
       </div>
     </div>
