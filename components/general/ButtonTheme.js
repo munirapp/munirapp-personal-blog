0eb20859f6540dd6cfad2e299b4e7ab6f1cb5cfa
munirapp <softwaremakassar@gmail.com>
2020-07-04 12:33:33 +0800

    setup black & white theme 3

---
+++ components/general/ButtonTheme.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+export default function ButtonTheme() {
+  const light_theme = {
+    buttonStyle: {
+      className: "btn btn-model3",
+      text: "Night Mode",
+      icon: "☀️",
+    },
+    elementStyle: { color: "white", textColor: "black" },
+  };
+  const dark_theme = {
+    buttonStyle: {
+      className: "btn btn-model1",
+      text: "Light Mode",
+      icon: "🌙",
+    },
+    elementStyle: { color: "#121212", textColor: "#fff" },
+  };
+
+  const [buttonStyle, setButtonStyle] = useState(light_theme.buttonStyle);
+
+  const handleClick = () => {
+    const element = document.documentElement;
+    const selected_theme = getComputedStyle(element).getPropertyValue(
+      "--theme-color"
+    );
+
+    if (!selected_theme || selected_theme == "white") {
+      element.style.setProperty("--theme-color", dark_theme.elementStyle.color);
+      element.style.setProperty(
+        "--theme-text-color",
+        dark_theme.elementStyle.textColor
+      );
+      setButtonStyle(dark_theme.buttonStyle);
+    } else {
+      element.style.setProperty(
+        "--theme-color",
+        light_theme.elementStyle.color
+      );
+      element.style.setProperty(
+        "--theme-text-color",
+        light_theme.elementStyle.textColor
+      );
+      setButtonStyle(light_theme.buttonStyle);
+    }
+  };
+
+  return (
+    <button className={buttonStyle.className} onClick={handleClick}>
+      {buttonStyle.icon} <span>{buttonStyle.text}</span>
+    </button>
+  );
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,34 +1,16 @@
+import ButtonTheme from "../../components/general/ButtonTheme";
+
 export default function Blog() {
-  const handleClick = () => {
-    const light_theme = { color: "white", textColor: "black" };
-    const dark_theme = { color: "black", textColor: "white" };
-
-    const element = document.documentElement;
-    const selected_theme = getComputedStyle(element).getPropertyValue(
-      "--theme-color"
-    );
-
-    if (!selected_theme || selected_theme == "white") {
-      element.style.setProperty("--theme-color", "black");
-      element.style.setProperty("--theme-color-text", "white");
-    } else {
-      element.style.setProperty("--theme-color", "white");
-      element.style.setProperty("--theme-color-text", "black");
-    }
-
-    console.log(selected_theme);
-  };
-
   return (
     <div>
       <div className="container">
-        <div className="flex">
+        <div className="flex items-center p-5">
           <a href="/">
             <span>Back to Home</span>
           </a>
-          <button className="btn btn-model2 ml-auto" onClick={handleClick}>
-            Go to Night Mode
-          </button>
+          <div className="ml-auto">
+            <ButtonTheme />
+          </div>
         </div>
       </div>
     </div>
