4ad8adced8cd66fc87fafccb9244eef30f9016f9
munirapp <softwaremakassar@gmail.com>
2020-07-06 13:14:33 +0800

    update minor changes

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c651d6d1d69e3925c036";
+/******/ 	var hotCurrentHash = "d8fae130751351e4e1d5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
