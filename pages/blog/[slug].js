926f217ab7d792e1fba59c881ac8d538a67237f6
munirapp <softwaremakassar@gmail.com>
2020-07-08 10:32:27 +0800

    setup markdown cheatsheet

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,45 +1,71 @@
+import { Component } from "react";
 import Head from "next/head";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import marked from "marked";
+import hljs from "highlight.js";
 
 import BlogHeader from "../../components/general/BlogHeader";
 import GeneralFooter from "../../components/general/GeneralFooter";
 
-const BlogSlug = ({ slug, content, meta }) => {
-  return (
-    <div>
-      <Head>
-        <title>{meta.title}</title>
-        <meta name="description" content={meta.desc} />
-      </Head>
+class BlogDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-      <BlogHeader linkBack="/blog" textBack="Back to Blog" />
+  componentDidMount() {
+    document.querySelectorAll("pre code").forEach((block) => {
+      hljs.highlightBlock(block);
+    });
+  }
 
-      <div className="blog-wrapper">
-        <div className="blog-header">
-          <div className="title">{meta.title}</div>
-          <div className="desc">
-            Author: {meta.author} | Date: {meta.date}
+  render() {
+    const { content, meta } = this.props;
+
+    return (
+      <div>
+        <Head>
+          <title>{meta.title}</title>
+          <meta name="description" content={meta.desc} />
+        </Head>
+
+        <BlogHeader linkBack="/blog" textBack="Back to Blog" />
+
+        <div className="blog-wrapper">
+          <div className="blog-header">
+            <div className="title">{meta.title}</div>
+            <div className="desc">
+              Author: {meta.author} | Date: {meta.date}
+            </div>
+            <img
+              src={meta.image}
+              alt={meta.title}
+              className="animate__animated animate__zoomIn"
+            />
           </div>
-          <img
-            src={meta.image}
-            alt={meta.title}
-            className="animate__animated animate__zoomIn"
-          />
-        </div>
-        <div className="container">
-          <div className="blog-body">
-            <div dangerouslySetInnerHTML={{ __html: content }}></div>
+          <div className="container">
+            <div className="blog-body">
+              <div dangerouslySetInnerHTML={{ __html: content }}></div>
+            </div>
           </div>
         </div>
+
+        <GeneralFooter />
       </div>
+    );
+  }
+}
 
-      <GeneralFooter />
-    </div>
-  );
-};
+// const BlogSlug = ({ slug, content, meta }) => {
+//   document.querySelectorAll("pre code").forEach((block) => {
+//     hljs.highlightBlock(block);
+//   });
+
+//   return (
+
+//   );
+// };
 
 export const getStaticPaths = async () => {
   const files = fs.readdirSync("data/artikel");
@@ -66,4 +92,4 @@ export const getStaticProps = async ({ params: { slug } }) => {
   };
 };
 
-export default BlogSlug;
+export default BlogDetail;
