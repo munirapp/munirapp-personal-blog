61af5a86c5710362d37a9439c75d25fee088d3e5
munirapp <softwaremakassar@gmail.com>
2023-05-19 22:35:20 +0800

    fix: improve articles format source

---
+++ src/interfaces/index.ts
@@ -0,0 +1,27 @@
+interface AstroPaginateOptions {
+  params?: {
+    [key: string]: string | number
+  }
+  pageSize?: number
+}
+
+interface AstroPaginateResult {
+  data: [any]
+  start: number
+  end: number
+  size: number
+  total: number
+  currentPage: number
+}
+
+type AstroPaginate = (data?: any[], options?: AstroPaginateOptions) => AstroPaginateResult
+
+export interface AstroGetStaticPathsParams {
+  paginate?: AstroPaginate
+}
+
+export interface Posts {
+  title: string
+  url: string
+  description: string
+}
